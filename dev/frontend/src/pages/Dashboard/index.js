069255a2015f1c0b4d9ae7fa3cf830b60dd696b7
Prisma MBP 103 <prisma@Assis.local>
2019-10-03 17:35:14 -0300

    frontend

--- dev/frontend/src/pages/Dashboard/index.js
+++ dev/frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 import './style.css'
@@ -29,6 +30,10 @@ export default function Dashboard() {
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new">
+                <button className="btn">Cadastrar novo Spot</button>
+            </Link>
         </>
     );
 }
